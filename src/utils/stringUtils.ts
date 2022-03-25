0f5d318746ab3d0a139514fb7416e6d1efb2db8d
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-25 17:54:36 +0530

    Update stringUtils.ts

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,3 +3,17 @@ export function camelize(str: string) {
     .toLowerCase()
     .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
 }
+
+export function capitalize(str: string) {
+  return str.replace(/(^\w{1})|(\s+\w{1})/g, (letter: string) =>
+    letter.toUpperCase()
+  );
+}
+
+export function split(str: string) {
+  return str.replace(/([a-z])([A-Z])/g, "$1 $2");
+}
+
+export function camelToCapitalize(str: string) {
+  return capitalize(split(str));
+}
