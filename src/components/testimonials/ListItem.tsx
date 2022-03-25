6f5734f20a0f2ca1215b68e84325dc1f97b38a50
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-25 18:33:01 +0530

    Add hover state to testimonials ListItem component

--- src/components/testimonials/ListItem.tsx
+++ src/components/testimonials/ListItem.tsx
@@ -12,7 +12,7 @@ const TestimonialsListItem: React.FC<TestimonialsApiRespResult> = ({
   created_at,
 }) => {
   return (
-    <div className="grid grid-cols-testimonial gap-6 items-center text-left px-6 py-2 bg-white text-gray-700">
+    <a className="grid grid-cols-testimonial gap-6 items-center text-left px-6 py-2 bg-white text-gray-700 hover:bg-gray-200 cursor-pointer">
       <Logo src={track.icon_url} alt={track.title} />
       <Logo src={mentor.avatar_url} alt={mentor.handle} />
       <div className="flex flex-col">
@@ -26,7 +26,7 @@ const TestimonialsListItem: React.FC<TestimonialsApiRespResult> = ({
       </p>
       <p className="text-right mr-4">{formatTimeToRelative(created_at)}</p>
       <Icon src={chevronRight} alt={"Icon to open the testimonial"} />
-    </div>
+    </a>
   );
 };
 
