15b5a24a74fb6903f4d17b76d7fcd57478f9c6cc
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-25 17:56:22 +0530

    Create basic elements

---
+++ src/elements/Search.tsx
@@ -0,0 +1,27 @@
+import searchIcon from "../assets/images/search.svg";
+import Icon from "./Icon";
+
+type SearchProps = {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder: string;
+};
+
+const Search: React.FC<SearchProps> = ({ value, onChange, placeholder }) => (
+  <label className="relative block ">
+    <span className="sr-only">{placeholder}</span>
+    <div className="absolute top-3 left-3 items-center">
+      <Icon src={searchIcon} alt={`${placeholder} icon`} />
+    </div>
+    <input
+      className="placeholder:italic placeholder:text-slate-400 block bg-gray-200 w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+      placeholder={placeholder}
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      name="search"
+    />
+  </label>
+);
+
+export default Search;
