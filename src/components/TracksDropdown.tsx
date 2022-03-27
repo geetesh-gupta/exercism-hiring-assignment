2bab0b658c4d61977ea6ff9aae865816b4bab25e
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-27 22:39:55 +0530

    Decrease TracksDropdown height on mobile

--- src/components/TracksDropdown.tsx
+++ src/components/TracksDropdown.tsx
@@ -20,7 +20,7 @@ const TracksDropdown: React.FC<TracksDropdownProps> = ({
         title: selectedTrack.title,
       }}
       showLogo
-      listClassName="h-96 overflow-y-scroll"
+      listClassName="h-48 md:h-96 overflow-y-scroll"
     >
       {tracks.map((track) => (
         <DropdownListItem
