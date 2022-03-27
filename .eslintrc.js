9b290e9fcebb9793024ed1ff7e129db08fb0c0d0
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-27 23:38:00 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
   },
 };
