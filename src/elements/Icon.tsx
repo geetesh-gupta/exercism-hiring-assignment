9d985c556f804e02827268b0bf45e7d71cc8fc14
Geetesh Gupta <geeteshgupta98@gmail.com>
2022-03-27 14:58:24 +0530

    Create IconWithBadge element

--- src/elements/Icon.tsx
+++ src/elements/Icon.tsx
@@ -1,3 +1,5 @@
+import Badge from "./Badge";
+
 export enum IconSizes {
   xs = "w-4",
   sm = "w-8",
@@ -27,6 +29,24 @@ const Icon: React.FC<IconProps> = ({
         rounded ? "rounded-full" : ""
       }`}
     />
+  );
+};
+
+export enum IconBadgeType {
+  dot,
+  number,
+}
+
+export const IconWithBadge: React.FC<IconProps> = ({
+  src,
+  alt,
+  rounded = false,
+  size = IconSizes.md,
+}) => {
+  return (
+    <Badge>
+      <Icon size={size} src={src} alt={alt} rounded={rounded} />
+    </Badge>
   );
 };
 
